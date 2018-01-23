19e3d0da535f68a73d3f7155b392a87b9adccc90
chezi008 <chezi008@qq.com>
2018-01-23 19:38:29 +0800

    修改读取文件的模式

--- app/src/main/java/com/chezi008/mp4muxerdemo/file/H264ReadRunable.java
+++ app/src/main/java/com/chezi008/mp4muxerdemo/file/H264ReadRunable.java
@@ -6,7 +6,6 @@ import java.io.DataInputStream;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.nio.ByteBuffer;
 
 /**
  * 描述：用来将本地h264数据分割成一帧一帧的数据
@@ -15,107 +14,62 @@ import java.nio.ByteBuffer;
  */
 
 public class H264ReadRunable implements Runnable {
-    String TAG = getClass().getSimpleName();
+    private static final int READ_BUFFER_SIZE = 1024 * 5;
+    private static final int BUFFER_SIZE = 1024 * 1024;
+
+    private String TAG = getClass().getSimpleName();
     private H264ReadListener h264ReadListener;
-    private boolean mStopFlag;
     private DataInputStream mInputStream;
-    long startMs = System.currentTimeMillis();
-    long timeoutUs = 10000;
-    byte[] marker0 = new byte[]{0, 0, 0, 1};
-    byte[] dummyFrame = new byte[]{0x00, 0x00, 0x01, 0x20};
-    byte[] streamBuffer = new byte[1024 * 1024 * 5];
 
     public void setH264ReadListener(H264ReadListener h264ReadListener) {
         this.h264ReadListener = h264ReadListener;
     }
+
+    private byte[] buffer;
 
     @Override
     public void run() {
         try {
             Log.d(TAG, "run: " + FileConstant.h264FilePath);
             mInputStream = new DataInputStream(new FileInputStream(FileConstant.h264FilePath));
-            while (true) {
-                int length = mInputStream.available();
-                if (length > 0) {
-                    int count = mInputStream.read(streamBuffer);
-                    mStopFlag = false;
-                    int bytes_cnt = 0;
-                    while (mStopFlag == false) {
-                        bytes_cnt = streamBuffer.length;
-                        if (bytes_cnt == 0) {
-                            streamBuffer = dummyFrame;
-                        }
+            buffer = new byte[BUFFER_SIZE];
 
-                        int startIndex = 0;
-                        int remaining = bytes_cnt;
-                        while (true) {
-                            if (remaining == 0 || startIndex >= remaining) {
-                                break;
-                            }
-                            int nextFrameStart = KMPMatch(marker0, streamBuffer, startIndex + 2, remaining);
-                            if (nextFrameStart == -1) {
-                                nextFrameStart = remaining;
-                            } else {
-                            }
-                            byte[] frameData = new byte[nextFrameStart - startIndex];
-                            System.arraycopy(streamBuffer, startIndex, frameData, 0, frameData.length);
-                            if (h264ReadListener == null) {
-                                throw new NullPointerException("readFle callback is null");
-                            }
-                            h264ReadListener.onFrameData(frameData);
-                            startIndex = nextFrameStart;
-                        }
-                        mStopFlag = true;
-                        h264ReadListener.onStopRead();
-                        Log.d("haha", "decodeLoop: end");
+            int readLength;
+            int naluIndex = 0;
+            int offset = 0;
+            int bufferLength = 0;
+
+            while ((readLength = mInputStream.read(buffer, offset, READ_BUFFER_SIZE)) > 0) {
+
+                bufferLength += readLength;
+                offset = bufferLength;
+                for (int i = 5; i < bufferLength - 4; i++) {
+                    if (buffer[i] == 0x00 &&
+                            buffer[i + 1] == 0x00 &&
+                            buffer[i + 2] == 0x00 &&
+                            buffer[i + 3] == 0x01) {
+                        naluIndex = i;
+//                        Log.d(TAG, "run: naluIndex:"+naluIndex);
+                        byte[] naluBuffer = new byte[naluIndex];
+                        System.arraycopy(buffer,0,naluBuffer,0,naluIndex);
+                        h264ReadListener.onFrameData(naluBuffer);
+                        offset = bufferLength-naluIndex;
+                        bufferLength -=naluIndex;
+                        System.arraycopy(buffer,naluIndex,buffer,0,offset);
+                        i = 5;
+                        Thread.sleep(30);
                     }
                 }
+
             }
+            h264ReadListener.onStopRead();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
-        }
-    }
-
-    int KMPMatch(byte[] pattern, byte[] bytes, int start, int remain) {
-        try {
-            Thread.sleep(30);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        int[] lsp = computeLspTable(pattern);
-
-        int j = 0;  // Number of chars matched in pattern
-        for (int i = start; i < remain; i++) {
-            while (j > 0 && bytes[i] != pattern[j]) {
-                // Fall back in the pattern
-                j = lsp[j - 1];  // Strictly decreasing
-            }
-            if (bytes[i] == pattern[j]) {
-                // Next char matched, increment position
-                j++;
-                if (j == pattern.length)
-                    return i - (j - 1);
-            }
-        }
-
-        return -1;  // Not found
-    }
-
-    int[] computeLspTable(byte[] pattern) {
-        int[] lsp = new int[pattern.length];
-        lsp[0] = 0;  // Base case
-        for (int i = 1; i < pattern.length; i++) {
-            // Start by assuming we're extending the previous LSP
-            int j = lsp[i - 1];
-            while (j > 0 && pattern[i] != pattern[j])
-                j = lsp[j - 1];
-            if (pattern[i] == pattern[j])
-                j++;
-            lsp[i] = j;
-        }
-        return lsp;
     }
 
     public interface H264ReadListener {
